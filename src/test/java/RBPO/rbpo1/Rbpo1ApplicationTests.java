cb486e3c4bc0bed332514cadbf054504a8a847bd
Stalker <137778635+Goliath7@users.noreply.github.com>
2025-10-24 21:18:32 +0300

    Add files via upload

---
+++ src/test/java/RBPO/rbpo1/Rbpo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package RBPO.rbpo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rbpo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
